d5ccde2f1d3c4e966a8818807a5aafa29ba18d92
nexusctc <hr@nexusctc.com>
2026-08-11 15:15:18 +0530

    set the folder structure

--- frontend-v1/src/pages/public/Home.jsx
+++ frontend-v1/src/pages/public/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from '../../components/home/Hero'
 import Faq from '../../components/home/Faq'
+import Needs from '../../components/home/Needs'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <Needs />
       <Faq/>
       
     </div>
